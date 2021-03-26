1b00152cbc50e48d8af3043f6a63592b403e2716
Suleika Tentua <suleika_t@hotmail.com>
2021-03-26 10:27:44 +0100

    answers opdracht basics arrow functions

---
+++ script.js
@@ -0,0 +1,20 @@
+// Deel A: 
+const ikrockArrowFunctions = () => console.log('Joe, ik rock de arrow function');
+ikrockArrowFunctions();
+
+// Deel B: 
+const fivePlusSeven = () => 5 + 7;
+fivePlusSeven();
+
+// Deel C: 
+const myFunction = (a,b) => a + b;
+myFunction();
+
+// Deel D: 
+const addFive = a => a + 5;
+addFive();
+
+// Deel E: 
+const createObject = () => ({greeting: "hoi"});
+createObject();
+
